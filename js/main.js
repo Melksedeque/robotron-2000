4c7d51535faf32146af9fddb896b63c6c9632038
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-05-15 12:13:35 -0300

    Função de manipulaAtributos() e atualizaEstatisticas() criadas para edição do Robotron2000

--- js/main.js
+++ js/main.js
@@ -1,12 +1,57 @@
-const robotron = document.querySelector("#robotron");
-
-robotron.addEventListener("click", (e) => {
-    console.log(e);
-});
-
-function dizOi(nome) {
-    console.log("Oi, " + nome)
-    console.log("Bem-vindo ao Robotron 2000!")
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5,
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20,
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -4,
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43,
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2,
+    },
 }
 
-dizOi("Melk");
+controle.forEach((e) => {
+    e.addEventListener("click", (e) => {
+        manipulaAtributos(e.target.dataset.controle, e.target.parentNode)
+        atualizaEstatisticas(e.target.dataset.peca)
+    })
+})
+
+function manipulaAtributos(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]')
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    }
+    else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca) {
+    estatisticas.forEach((e) => {
+        e.textContent = parseInt(e.textContent) + pecas[peca][e.dataset.estatistica]
+    })
+}
